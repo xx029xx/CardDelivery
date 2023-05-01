db6ab37bcf5a959986917d01f993fb24f5426549
Natalia Zvereva <xx029xx@ya.ru>
2023-05-01 11:04:15 +0300

    chore: test passed

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -32,36 +32,4 @@ public class CardDeliveryTest {
                 .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
                         Duration.ofSeconds(15));                        //Загрузка не более 15 секунд
     }
-
-    public void formSubmittedSuccessfully2() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Уфа");
-        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
-        String verificationDate = LocalDate.now() .plusDays(0)         //Текущая дата плюс 0 дня
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
-        $("[data-test-id=date] input").setValue(verificationDate);
-        $("[data-test-id=name] input").setValue("Иван Иванов-Иванов");
-        $("[data-test-id=phone] input").setValue("+79999999999");
-        $("[data-test-id=agreement]").click();
-        $x("//*[text()=\"Забронировать\"]").click();
-        $("[data-test-id=notification]")
-                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
-                        Duration.ofSeconds(5));                        //Загрузка не более 5 секунд
-    }
-
-    public void formSubmittedSuccessfully3() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Улан-Удэ");
-        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
-        String verificationDate = LocalDate.now() .plusDays(1)         //Текущая дата плюс 1 день
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
-        $("[data-test-id=date] input").setValue(verificationDate);
-        $("[data-test-id=name] input").setValue("Иван Иванов");
-        $("[data-test-id=phone] input").setValue("+71111111111");
-        $("[data-test-id=agreement]").click();
-        $x("//*[text()=\"Забронировать\"]").click();
-        $("[data-test-id=notification]")
-                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
-                        Duration.ofSeconds(10));                        //Загрузка не более 10 секунд
-    }
 }
