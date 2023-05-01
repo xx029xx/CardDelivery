851b40ae6f689369ff0ccfd1f914f06b035cd5c6
Natalia Zvereva <xx029xx@ya.ru>
2023-05-01 10:42:22 +0300

    chore: one test

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,67 @@
+package ru.netology.selenide;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    @Test
+    public void formSubmittedSuccessfully() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Майкоп");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String verificationDate = LocalDate.now() .plusDays(3)         //Текущая дата плюс 3 дня
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
+        $("[data-test-id=date] input").setValue(verificationDate);
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()=\"Забронировать\"]").click();
+        $("[data-test-id=notification]")
+                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                        Duration.ofSeconds(15));                        //Загрузка не более 15 секунд
+    }
+
+    public void formSubmittedSuccessfully2() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Уфа");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String verificationDate = LocalDate.now() .plusDays(0)         //Текущая дата плюс 0 дня
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
+        $("[data-test-id=date] input").setValue(verificationDate);
+        $("[data-test-id=name] input").setValue("Иван Иванов-Иванов");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()=\"Забронировать\"]").click();
+        $("[data-test-id=notification]")
+                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                        Duration.ofSeconds(5));                        //Загрузка не более 5 секунд
+    }
+
+    public void formSubmittedSuccessfully3() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Улан-Удэ");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String verificationDate = LocalDate.now() .plusDays(1)         //Текущая дата плюс 1 день
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
+        $("[data-test-id=date] input").setValue(verificationDate);
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+71111111111");
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()=\"Забронировать\"]").click();
+        $("[data-test-id=notification]")
+                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                        Duration.ofSeconds(10));                        //Загрузка не более 10 секунд
+    }
+}
